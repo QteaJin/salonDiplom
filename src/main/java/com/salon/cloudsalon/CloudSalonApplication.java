8e14f8f879edd3dadb205bacbb04d6d2ab07f0e0
Sergey_Jushenko <33098674+od081274gil@users.noreply.github.com>
2019-01-11 13:14:28 +0200

    create project

---
+++ src/main/java/com/salon/cloudsalon/CloudSalonApplication.java
@@ -0,0 +1,14 @@
+package com.salon.cloudsalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSalonApplication.class, args);
+	}
+
+}
+
