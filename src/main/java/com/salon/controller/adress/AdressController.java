2bccd700e5acd12bc85f236a71de8a07109bd6e1
QteaJin <fartukov@gmail.com>
2021-06-05 10:25:04 +0300

    ChangeTest

--- src/main/java/com/salon/controller/adress/AdressController.java
+++ src/main/java/com/salon/controller/adress/AdressController.java
@@ -46,6 +46,6 @@ public class AdressController {
 	@RequestMapping(method = RequestMethod.PUT)
 	public AdressBean updateAdress(@RequestBody AdressBean adressBean) {
 		return adressService.update(adressBean);
-	}
+	} //
 
 }
